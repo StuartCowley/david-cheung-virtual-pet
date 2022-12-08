83862e8863a00efbbae4bf67b8130dc33b8f9e57
David Cheung <davidcheungtw@gmail.com>
2022-12-08 21:57:01 +0000

    add growUp method

--- src/pet.js
+++ src/pet.js
@@ -1,7 +1,14 @@
 function Pet(name){
   if (!(this instanceof Pet))
     return new Pet(name)
-  this.name = name
+  this.name = name;
+  this.age = 0;
 }
 
+// all pets will share the same growUp method.
+// save memory
+//
+Pet.prototype.growUp = function() {
+  this.age += 1;
+};
 module.exports = Pet;
