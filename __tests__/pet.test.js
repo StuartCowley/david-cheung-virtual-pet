45d81b8db27bccf36011063ac5330e59b57a135e
David Cheung <davidcheungtw@gmail.com>
2022-12-08 22:29:16 +0000

    refactor

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -13,6 +13,9 @@ describe('constructor', () => {
   it('has a initial age of 0', () => {
     const pet = new Pet('Fido');
     expect(pet.age).toEqual(0);
+    expect(pet.hunger).toEqual(0);
+    expect(pet.fitness).toEqual(10);
+
   });
 
 });
@@ -24,5 +27,32 @@ describe('growUp', () => {
     pet.growUp();
 
     expect(pet.age).toEqual(1);
+    expect(pet.hunger).toEqual(5);
+    expect(pet.fitness).toEqual(7);
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(2);
+    expect(pet.hunger).toEqual(10);
+    expect(pet.fitness).toEqual(4);
+
+  });
+});
+
+describe('walk', () => {
+  it('increments the fitness by 4, but max 10', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+
+    pet.growUp();
+    expect(pet.fitness).toEqual(4);
+
+    pet.walk();
+    expect(pet.fitness).toEqual(8);
+
+    pet.walk();
+    expect(pet.fitness).toEqual(10);
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -2,13 +2,28 @@ function Pet(name){
   if (!(this instanceof Pet))
     return new Pet(name)
   this.name = name;
-  this.age = 0;
+  this.age = 0;         // initial value
+  this.hunger = 0;      // initial value
+  this.fitness = 10;    // initial value
 }
 
 // all pets will share the same growUp method.
 // save memory
 //
 Pet.prototype.growUp = function() {
+  const moreHunger = 5;
+  const lessFitness = 3;
   this.age += 1;
+  this.hunger += moreHunger;
+  this.fitness -= lessFitness;
 };
+
+// give them some exercise to boost their fitness levels
+Pet.prototype.walk = function() {
+  const maxFitness = 10;
+  const moreFitness = 4;
+  this.fitness += moreFitness;
+  if (this.fitness > maxFitness) {this.fitness = maxFitness;};
+};
+
 module.exports = Pet;
